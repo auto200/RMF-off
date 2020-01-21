41c585b3a5cdf006e546f0c8ca47db2f5d8f4e71
auto200 <michal.warac@gmail.com>
2020-01-21 01:41:05 +0100

    Move themes to separate file

---
+++ src/utils/themes.js
@@ -0,0 +1,18 @@
+export const darkTheme = {
+  colors: {
+    primary: "#181a1b",
+    secondary: "#393939",
+    regularText: "#dcdde1",
+    highlightText: "#ffffff"
+  },
+  imgBrightness: 0.7
+};
+export const lightTheme = {
+  colors: {
+    primary: "#ffffff",
+    secondary: "#dcdde1",
+    regularText: "#393939",
+    highlightText: "#181a1b"
+  },
+  imgBrightness: 1
+};
