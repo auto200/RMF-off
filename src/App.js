81840f1e4004c23d454c25935e8f5ce7c916b0f6
auto200 <michal.warac@gmail.com>
2020-01-21 01:50:51 +0100

    Integrate socketIO, move themes to separate file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import axios from "axios";
+import { lightTheme, darkTheme } from "./utils/themes";
 import Header from "./components/Header";
 import Tails from "./components/Tails";
 import Player from "./Player";
+import socketIO from "socket.io-client";
 
 const GlobalStyle = createGlobalStyle`
   html, body{
@@ -18,48 +19,43 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 `;
-const darkTheme = {
-  colors: {
-    primary: "#181a1b",
-    secondary: "#393939",
-    regularText: "#dcdde1",
-    highlightText: "#ffffff"
-  },
-  imgBrightness: 0.7
-};
-const lightTheme = {
-  colors: {
-    primary: "#ffffff",
-    secondary: "#dcdde1",
-    regularText: "#393939",
-    highlightText: "#181a1b"
-  },
-  imgBrightness: 1
-};
 
 const filterTypes = {
   stationName: "Nazwa stacji",
   artist: "Wykonawca",
   songName: "Nazwa piosenki"
 };
+
+const socket = socketIO("http://localhost:3000");
+
 function App() {
   const [colorTheme, setColorTheme] = useState("dark");
   const [tails, setTails] = useState([]);
+  window.tails = tails;
   const [filtredTails, setFiltredTails] = useState([]);
   const [currentRadioUrl, setCurrentRadioUrl] = useState("");
   const [[currentFilterType, filterValue], setFilter] = useState([
     "stationName",
     ""
   ]);
-  window.tails = tails;
 
   useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get("http://localhost:3000");
+    socket.on("initialData", data => {
       setTails(data);
-      setFiltredTails(data);
-    };
-    getData();
+    });
+    socket.on("dataUpdate", data => {
+      console.log(data);
+      setTails(prev => {
+        const newTails = prev.map(tail => {
+          const modifiedTail = data.find(obj => obj.id === tail.id);
+          if (modifiedTail) {
+            return { ...tail, ...modifiedTail };
+          }
+          return tail;
+        });
+        return newTails;
+      });
+    });
   }, []);
 
   useEffect(() => {
@@ -69,7 +65,7 @@ function App() {
         tail[currentFilterType].toLowerCase().includes(filterVal)
       )
     );
-  }, [filterValue]);
+  }, [tails, filterValue, currentFilterType]);
 
   const toggleTheme = () =>
     setColorTheme(prev => (prev === "dark" ? "light" : "dark"));
