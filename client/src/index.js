79e80b88a69e6130ef349e50cf3ad39e4843e2be
auto200 <michal.warac@gmail.com>
2021-02-25 21:25:31 +0100

    Set dark theme as default

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./utils/theme";
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>,
   document.getElementById("root")
--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import { headerHeight, playerHeight } from "./constants";
 
 export default extendTheme({
+  config: { initialColorMode: "dark" },
   styles: {
     global: {
       "#root": {
