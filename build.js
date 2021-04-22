f04af542cda36779c54340feb9410bb2d245d8ec
auto200 <michal.warac@gmail.com>
2021-04-22 15:33:33 +0200

    Make it buildable

--- build.js
+++ build.js
@@ -1,8 +1,9 @@
 const shell = require("shelljs");
 
 shell.exec("npm run build:client");
+shell.exec("npm run build:server");
 shell.rm("-rf", "build");
 shell.mkdir("-p", "build");
-shell.cp("./server/app.js", "./build");
+shell.cp("-R", "./server/build/*", "./build");
 shell.cp("./server/package.json", "./build");
 shell.cp("-R", "./client/build", "./build/public");
