1349c088a3425bc06d56997f937e909ff5f185e3
auto200 <michal.warac@gmail.com>
2021-04-27 11:02:57 +0200

    Boost performance by rendering only body of visible stations

--- client/src/components/Stations/Station/index.tsx
+++ client/src/components/Stations/Station/index.tsx
@@ -39,9 +39,10 @@ const Station: React.FC<IProps> = ({
   const { ref, inView } = useInView();
   const [inViewBackground, setInViewBackground] = useState<string>("");
   const activeTailBackground = useColorModeValue("gray.50", "gray.900");
+  const coverFlterOpacity = useColorModeValue(0, 0.3);
+  const songTitleColor = useColorModeValue("gray.700", "gray.300");
 
   const query = encodeURIComponent(`${song.artist} - ${song.name}`);
-  console.log("update");
   useEffect(() => {
     if (inView && song.cover !== inViewBackground) {
       setInViewBackground(song.cover);
@@ -51,6 +52,7 @@ const Station: React.FC<IProps> = ({
   return (
     <Flex
       maxW="500px"
+      h="410px"
       flexDir="column"
       alignItems="center"
       p={1}
@@ -60,112 +62,116 @@ const Station: React.FC<IProps> = ({
       transition="background 0.5s"
       ref={ref}
     >
-      <Heading isTruncated color="blue.600" maxW="95%" title={name} mb={1}>
-        {name}
-      </Heading>
-      <AnimatePresence exitBeforeEnter>
-        <CoverContainer
-          key={inViewBackground}
-          w="full"
-          h="300px"
-          pos="relative"
-          bgPos="center"
-          bgSize="cover"
-          bgImage={`url(${inViewBackground})`}
-          sx={
-            {
-              //prevent div flash on tap
-              //https://stackoverflow.com/questions/6211959/html5-div-flashes-on-tap
-              WebkitTapHighlightColor: "rgba(0,0,0,0)",
-              //brightness filter
-              "&::before": {
-                content: "''",
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                backgroundColor: `rgba(0, 0, 0, ${useColorModeValue(0, 0.3)})`,
-                transition: "background-color 0.3s",
-              },
-            } as React.CSSProperties
-          }
-          //motion props
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <PlayerStateIcon
-            icon={!isActive ? <FaPlay /> : undefined}
-            onClick={handleCoverClick}
-            playerState={playerState}
-            boxSize="full"
-            fontSize="70px"
-            color="gray.100"
-            opacity={isActive ? 0.7 : 0}
-            _hover={{ cursor: "pointer", opacity: isActive ? 1 : 0.7 }}
-            _focus={{ opacity: 0.7 }}
-            transition="opacity 0.3s"
-            aria-label={`${isActive ? "Pause" : "Play"} current station`}
-          />
-        </CoverContainer>
-      </AnimatePresence>
-      <Flex width="95%">
-        <Box flexGrow={1} isTruncated textAlign="center">
-          <Text
-            fontSize="2xl"
-            color={useColorModeValue("gray.700", "gray.300")}
-            isTruncated
-            py={song.artist ? 0 : 3}
-            title={song.name}
-          >
-            {song.name}
-          </Text>
-          <Heading
-            as="h3"
-            fontSize="md"
-            color="gray.500"
-            isTruncated
-            title={song.artist}
-          >
-            {song.artist}
+      {inView ? (
+        <>
+          <Heading isTruncated color="blue.600" maxW="95%" title={name} mb={1}>
+            {name}
           </Heading>
-        </Box>
-        {/* dots menu */}
-        <Menu isLazy>
-          <MenuButton
-            as={IconButton}
-            my="auto"
-            aria-label="Menu"
-            icon={<BiDotsVerticalRounded />}
-            isRound={true}
-            fontSize="2xl"
-            bg="transparent"
-          />
-          <MenuList py={0}>
-            <MenuItem
-              as={Link}
-              icon={<FaGoogle />}
-              h="50px"
-              command={((<BiLinkExternal />) as unknown) as string}
-              href={`https://www.google.com/search?q=${query}`}
-              isExternal
+          <AnimatePresence exitBeforeEnter>
+            <CoverContainer
+              key={inViewBackground}
+              w="full"
+              h="300px"
+              pos="relative"
+              bgPos="center"
+              bgSize="cover"
+              bgImage={`url(${inViewBackground})`}
+              sx={
+                {
+                  //prevent div flash on tap
+                  //https://stackoverflow.com/questions/6211959/html5-div-flashes-on-tap
+                  WebkitTapHighlightColor: "rgba(0,0,0,0)",
+                  //brightness filter
+                  "&::before": {
+                    content: "''",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: `rgba(0, 0, 0, ${coverFlterOpacity})`,
+                    transition: "background-color 0.3s",
+                  },
+                } as React.CSSProperties
+              }
+              //motion props
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
             >
-              Szukaj w Google
-            </MenuItem>
-            <MenuItem
-              as={Link}
-              icon={<FaYoutube />}
-              h="50px"
-              command={((<BiLinkExternal />) as unknown) as string}
-              href={`https://www.youtube.com/results?search_query=${query}`}
-              isExternal
-            >
-              Szukaj w YouTube
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Flex>
+              <PlayerStateIcon
+                icon={!isActive ? <FaPlay /> : undefined}
+                onClick={handleCoverClick}
+                playerState={playerState}
+                boxSize="full"
+                fontSize="70px"
+                color="gray.100"
+                opacity={isActive ? 0.7 : 0}
+                _hover={{ cursor: "pointer", opacity: isActive ? 1 : 0.7 }}
+                _focus={{ opacity: 0.7 }}
+                transition="opacity 0.3s"
+                aria-label={`${isActive ? "Pause" : "Play"} current station`}
+              />
+            </CoverContainer>
+          </AnimatePresence>
+          <Flex width="95%">
+            <Box flexGrow={1} isTruncated textAlign="center">
+              <Text
+                fontSize="2xl"
+                color={songTitleColor}
+                isTruncated
+                py={song.artist ? 0 : 3}
+                title={song.name}
+              >
+                {song.name}
+              </Text>
+              <Heading
+                as="h3"
+                fontSize="md"
+                color="gray.500"
+                isTruncated
+                title={song.artist}
+              >
+                {song.artist}
+              </Heading>
+            </Box>
+            {/* dots menu */}
+            <Menu isLazy>
+              <MenuButton
+                as={IconButton}
+                my="auto"
+                aria-label="Menu"
+                icon={<BiDotsVerticalRounded />}
+                isRound={true}
+                fontSize="2xl"
+                bg="transparent"
+              />
+              <MenuList py={0}>
+                <MenuItem
+                  as={Link}
+                  icon={<FaGoogle />}
+                  h="50px"
+                  command={((<BiLinkExternal />) as unknown) as string}
+                  href={`https://www.google.com/search?q=${query}`}
+                  isExternal
+                >
+                  Szukaj w Google
+                </MenuItem>
+                <MenuItem
+                  as={Link}
+                  icon={<FaYoutube />}
+                  h="50px"
+                  command={((<BiLinkExternal />) as unknown) as string}
+                  href={`https://www.youtube.com/results?search_query=${query}`}
+                  isExternal
+                >
+                  Szukaj w YouTube
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </>
+      ) : null}
     </Flex>
   );
 };
