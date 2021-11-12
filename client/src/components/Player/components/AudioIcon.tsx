5b0c633d709156304265001a93ecaed1dfa92088
auto200 <michal.warac@gmail.com>
2021-11-12 17:19:01 +0100

    Refactor player AudioIcon

--- client/src/components/Player/components/AudioIcon.tsx
+++ client/src/components/Player/components/AudioIcon.tsx
@@ -1,17 +1,34 @@
+import { IconButton } from "@chakra-ui/button";
 import { FaVolumeDown, FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 
-interface AudioIconProps {
-  volume: number;
-}
+const icons = {
+  high: FaVolumeUp,
+  mid: FaVolumeDown,
+  muted: FaVolumeMute,
+};
 
-const AudioIcon: React.FC<AudioIconProps> = ({ volume }) => {
-  if (volume === 0) {
-    return <FaVolumeMute />;
-  } else if (volume > 0 && volume < 51) {
-    return <FaVolumeDown />;
-  } else {
-    return <FaVolumeUp />;
-  }
+type AudioIconProps = {
+  volume: number;
+} & any;
+
+const AudioIcon: React.FC<AudioIconProps> = ({ volume, ...rest }) => {
+  let Icon = icons.mid;
+
+  if (volume > 50) Icon = icons.high;
+  else if (volume > 0) Icon = icons.mid;
+  else Icon = icons.muted;
+
+  return (
+    <IconButton
+      icon={<Icon cursor="pointer" />}
+      aria-label="Przełącz wyciszenie"
+      bg="transparent"
+      _hover={{ bg: "transparent" }}
+      _active={{ bg: "transparent" }}
+      fontSize="4xl"
+      {...rest}
+    />
+  );
 };
 
 export default AudioIcon;
